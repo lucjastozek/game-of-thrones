0abb8ec3c3d8ab4304b54bc932490dfaa2e9a9ef
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-07-24 12:20:29 +0200

    Build: log with workStack and queue

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ import { printBanner } from "./utils/bannerUtils";
 import { printPersonTreeAsAscii } from "./utils/toAsciiTree";
 
 async function mainProgram() {
-  printBanner("BARATHEON");
-  printPersonTreeAsAscii(createBaratheonTree());
+  // printBanner("BARATHEON");
+  // printPersonTreeAsAscii(createBaratheonTree());
 
-  printBanner("STARK");
-  printPersonTreeAsAscii(createStarkTree());
+  // printBanner("STARK");
+  // printPersonTreeAsAscii(createStarkTree());
 
-  // Uncomment to test your work..
+  //Uncomment to test your work..
 
   // printBanner("LANNISTER");
   // printPersonTreeAsAscii(createLannisterTree());
@@ -22,10 +22,10 @@ async function mainProgram() {
   // printBanner("All people in STARK")
   // logAllPeopleInTree(createStarkTree());
 
-  // printBanner("WITH STACK - british royals")
-  // logAllPeopleInTree(createBritishSuccessionTree());
-  // printBanner("WITH QUEUE - british royals")
-  // logAllPeopleInTreeWithQueue(createBritishSuccessionTree());
+  printBanner("WITH STACK - british royals")
+  logAllPeopleInTree(createBritishSuccessionTree());
+  printBanner("WITH QUEUE - british royals")
+  logAllPeopleInTreeWithQueue(createBritishSuccessionTree());
 
   // You'll need to add more test code for later exercises, too.
 }
--- src/treeLogging.ts
+++ src/treeLogging.ts
@@ -1,11 +1,38 @@
 import { Person } from "./personTypes";
+/*
+function logAllPeopleInTree(rootPerson)
+    workStack = empty stack of Person objects
+    push rootPerson onto workStack
 
+		while workStack is not empty: 
+        currentPerson = pop person off workStack
+        log the name property of currentPerson
+        push each element of currentPerson's children onto the workStack
+*/
 
 export function logAllPeopleInTree(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+    let workStack:Person[] = [];
+    workStack.push(topPerson);
+    while(workStack.length>0) {
+            const currentPerson = workStack.pop() as Person;
+            console.log(currentPerson.name);
+            workStack.push(...currentPerson.children);
+
+
+    }
+   
 }
 
 export function logAllPeopleInTreeWithQueue(topPerson: Person): void {
-    console.log("TODO!  First person is " + topPerson.name);
+    let queue: Person[] =[];
+    queue.push(topPerson);
+    while(queue.length>0) {
+        const currentPerson = queue.pop() as Person;
+        console.log(currentPerson.name);
+        queue.unshift(...currentPerson.children.reverse());
+
+
+}
+   
 }
 
