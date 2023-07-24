bd4af7bfeafc7997ba07fcfc083f4d4ee84ecfaf
lucjastozek <lucjastozek@gmail.com>
2023-07-24 11:09:56 +0200

    added stark

--- src/families/stark.ts
+++ src/families/stark.ts
@@ -1,6 +1,19 @@
 import { Person } from "../personTypes";
 
 export function createStarkTree(): Person {
+  const rickon: Person = { name: "Rickon", children: [] };
+  const bran: Person = { name: "Bran", children: [] };
+  const arya: Person = { name: "Arya", children: [] };
+  const sansa: Person = { name: "Sansa", children: [] };
+  const robb: Person = { name: "Robb", children: [] };
+  const jon: Person = { name: "Jon", children: [] };
+  const eddardChildren: Person[] = [rickon, bran, arya, sansa, robb, jon];
+  const eddard: Person = { name: "Eddard", children: eddardChildren };
+  const benjen: Person = { name: "Benjen", children: [] };
+  const lyanna: Person = { name: "Lyanna", children: [] };
+  const brandon: Person = { name: "Brandon", children: [] };
+  const rickardChildren: Person[] = [eddard, benjen, lyanna, brandon];
+  const rickard: Person = { name: "Rickard", children: rickardChildren };
 
-    return { name: "STARK: YOU NEED TO IMPLEMENT THIS", children: [] };
+  return rickard;
 }
